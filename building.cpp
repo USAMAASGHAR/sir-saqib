c6b14b54bc4551faed5ed2e5557591f322e48c6d
abddullahsultan <abdullahsultan448@gmail.co>
2018-12-18 23:17:17 +0500

    DOne N

--- building.cpp
+++ building.cpp
@@ -5,52 +5,49 @@ using namespace std;
  class building
  {
 protected:
-   int offices[10];
-   int N; //Max visitor capasity of building
+   static int N; //Max visitor capasity of building
    int Nf;//Each floor capacity
    int Ng;//ground_floor capacity
-   int No;//each office capacity
-
+   int No[10];//each office capacity
+   int E;
 public:
-  building(){N=0;}
-  building(int capacity)
+  building(){N=0; cout<<"A new building is ready for serving citizens! \n"}
+  void set_capasity(int capacity)
   {
     N=capacity;
+
   }
-
-
-
-
-
 
  };
 
- class ground_floor():public building
+ class ground_floor:public building
  {
  public:
    ground_floor()
     {Ng=N/2;}
- };
+};
 class first_floor:public building
 {
+public:
   first_floor()
-  {Nf=N/3;}
+  {Nf=rand()%(N/3);}
+
 };
 class second_floor:public building
 {
   second_floor()
-  {Nf=N/3;}
+  {Nf=rand()%N/3;}
 };
 
 class third_floor:public building
 {
   third_floor()
-  {Nf=N/3;}
+  {Nf=rand()%N/3;}
 };
 class fourth_floor:public building
 {
   fourth_floor()
-  {Nf=N/3;}
+  {Nf=rand()%N/3;}
 };
 class elevator:public building
 {
@@ -59,6 +56,9 @@ class elevator:public building
 
 int main()
 {
+  cout << "Enter capacity of Building" << '\n';
+  int capacity;
+  cin capacity; building obj(); obj.set_capasity(capacity);
 
-return 0;
+  return 0;
 }
