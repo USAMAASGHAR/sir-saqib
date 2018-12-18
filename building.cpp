0487427ad642b944b31da68e98b2d2d59b92a532
abddullahsultan <abdullahsultan448@gmail.co>
2018-12-18 18:02:24 +0500

    inherited

--- building.cpp
+++ building.cpp
@@ -11,6 +11,28 @@ public:
 
  };
 
+ class ground_floor():public building
+ {
+
+ };
+class first_floor:public building
+{
+
+};
+class second_floor:public building
+{
+
+};class third_floor:public building
+{
+
+};class fourth_floor:public building
+{
+
+};
+class elevator:public building
+{
+
+};
 
 int main()
 {
